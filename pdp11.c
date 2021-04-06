02d8b3d6a185e6142f69474a1ef74e585f16348b
nataha11 <kuzmina.nd@phystech.edu>
2021-04-07 00:56:49 +0300

    mem_dump tests

--- pdp11.c
+++ pdp11.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <errno.h>
+#include <stdint.h>
 
-typedef unsigned char byte;   //8 bit
-typedef unsigned int word;    //16 bit
+typedef uint8_t byte;   //8 bit
+typedef uint16_t word;    //16 bit
 typedef word Adress;
 
 #define MEMSIZE (64 * 1024)
@@ -13,15 +16,15 @@ byte b_read(Adress adr);
 void b_write (Adress adr, byte b);
 word w_read(Adress adr);
 void w_write(Adress adr, word w);
+void test_mem();
 
 void load_file();
 
-
-void test_mem();
+void mem_dump(Adress start, word n);
 
 int main() {
+
 	test_mem ();
-	load_file("test.o");
 	return 0;
 }
 
@@ -35,13 +38,13 @@ void test_mem() {
 
 	//write 2 bytes, read word
 	Adress a = 4;
-	byte b1 = 0xcb;
+	byte b = 0xcb;
 	b0 = 0x0a;
 	word w = 0xcb0a;
 	b_write(a, b0);
-	b_write(a + 1, b1);
+	b_write(a + 1, b);
 	word wres = w_read(a);
-	printf("ww/br \t %04hx=%02hhx%02hhx\n", wres, b1, b0);
+	printf("ww/br \t %04hx=%02hhx%02hhx\n", wres, b, b0);
 	assert(w == wres);
 
 	//read word, write word
@@ -51,6 +54,28 @@ void test_mem() {
 	word wreso = w_read(ad);
 	printf("ww/wr \t %04hx=%04hx\n", wo, wreso);
 	assert(wo == wreso);
+
+	//write word, read 2 bytes
+	Adress adr = 20;
+	word wor = 0xaaba;
+	w_write(adr, wor);
+	byte b1 = 0xaa;
+	byte b2 = 0xba;
+	byte bres2 = b_read(adr);
+	byte bres1 = b_read(adr + 1);
+	printf("ww/rb \t %04hx=%02hhx%02hhx\n", wor, bres1, bres2);
+	assert((bres1 == b1) && (bres2 == b2));
+
+	//ww wr отрицательные числа
+	word neg_w = -1;//255
+	w_write(100, neg_w);
+	word res = w_read(100);
+	printf("ww/wr \t %016hx=%016hx\n", neg_w, res);
+	assert(neg_w == res);
+
+	load_file("test.o");
+
+	mem_dump(0x40, 4);
 }
 
 byte b_read(Adress adr) {
@@ -68,18 +93,23 @@ word w_read(Adress a) {
 }
 
 void w_write(Adress adr, word w) {
-	mem[adr + 1] = (byte)(w >> 8);
-	mem[adr] = (byte)w;
-	
+	mem[adr] = w & 0xFF;//извлекаем первые 8 бит
+	mem[adr + 1] = (w >> 8) & 0xFF;//извлекаем следующие 8 бит
 }
 
 void load_file(const char * filname) {
 	FILE * fil = fopen(filname, "rb");
+	if (fil == NULL) {
+		printf("Oooops! Error!\n");
+		perror(filname);
+		exit(errno);
+	}
+
 	Adress a;
 	byte c;
 	word n;
 
-	while(2 == fscanf(fil, "%x%x", &a, &n)) {
+	while(2 == fscanf(fil, "%hx%hx", &a, &n)) {
 		printf("read of size %u\n", n);
 		for (unsigned int i = 0; i < n; i++) {
 				fscanf(fil, "%hhx", &c);
@@ -89,5 +119,11 @@ void load_file(const char * filname) {
 	}
 	printf("\n");
 	fclose(fil);
+}
+
+void mem_dump(Adress start, word n) {
+	for (unsigned int i = 0; i < n; i += 2) {
+		printf("%06o : %06o\n", start + i, w_read(start + i));
+	}
 }
 
